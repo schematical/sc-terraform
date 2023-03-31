77f9077179dcc1459ef792d900b8ee73b6fb0d1f
Your Name <you@example.com>
2023-03-31 16:39:25 -0500

    Tweaking base VPC

--- modules/sagemaker/build/src/run.js
+++ modules/sagemaker/build/src/run.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const { spawn } = require('child_process');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/ping', (req, res) => {
     console.log("PING")
