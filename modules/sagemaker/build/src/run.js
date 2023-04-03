dddac19e63863d1970ca908e23f615cb4c5d5d37
Your Name <you@example.com>
2023-04-03 08:46:49 -0500

    Tweaking base VPC

--- modules/sagemaker/build/src/run.js
+++ modules/sagemaker/build/src/run.js
@@ -2,15 +2,17 @@ const fs = require('fs');
 
 const { spawn } = require('child_process');
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express()
 const port = 8080
-
+app.use(bodyParser.json());
 app.get('/ping', (req, res) => {
     console.log("PING")
     res.send('Hello World!')
 })
 app.post('/invocations', async (req, res) => {
     console.log("req.body: ", req.body);
+    console.log("req.headers: ", req.headers);
     await runSpawn({
         path: '/home/ubuntu/src/run.sh',
         cmd: '/opt/conda/install/bin/conda',
