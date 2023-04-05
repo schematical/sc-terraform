60d89b9511da2edd27999d585b8d590bb7bd3081
Your Name <you@example.com>
2023-04-05 16:05:05 -0500

    Adding Docs

--- modules/sagemaker/build/src/train.sh
+++ modules/sagemaker/build/src/train.sh
@@ -3,6 +3,18 @@
 echo "The script was called with $# arguments."
 echo "The arguments are:  $1, $2, $3, $4, $5"
 
+
+echo "ls /opt/ml/"
+ls -la /opt/ml/
+echo "cat /opt/ml/input/config/inputdataconfig.json"
+cat /opt/ml/input/config/inputdataconfig.json
+echo "ls /opt/ml/input/data/train"
+ls -la /opt/ml/input/data/train
+
+echo "ls -la /opt/ml/checkpoints"
+ls -la /opt/ml/checkpoints
+
+echo "------------\n"
 cd /home/ubuntu/src/dreambooth
 
 echo "!!!!!!conda init bash"
@@ -10,7 +22,7 @@ echo "!!!!!!conda init bash"
 python /home/ubuntu/src/dreambooth/main.py \
   --base /home/ubuntu/src/dreambooth/configs/stable-diffusion/v1-finetune_unfrozen.yaml \
   -t \
-  --actual_resume  /opt/ml/model/model.pth \
+  --actual_resume  /opt/ml/checkpoints/model.pth \
   -n Test1 \
   --gpus '0,' \
   --data_root /home/ubuntu/src/images \
