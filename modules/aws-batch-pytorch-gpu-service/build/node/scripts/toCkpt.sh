6768ef2536efc2e4b136d857c65c9d3d0a104a62
schematical <mlea@schematical.com>
2023-05-02 17:35:27 -0500

    Finishing the rough model training data structure

--- modules/aws-batch-pytorch-gpu-service/build/node/scripts/toCkpt.sh
+++ modules/aws-batch-pytorch-gpu-service/build/node/scripts/toCkpt.sh
@@ -3,5 +3,8 @@
 model_path=$1
 ckpt_name=$(basename $model_path)
 ckpt_path="${ckpt_name}.ckpt"
-
+cd /home/ubuntu/src/dreambooth/examples/dreambooth/text-inversion-model
 python3 /home/ubuntu/node/scripts/convertToCkpt.py --model_path=$model_path --checkpoint_path=$ckpt_path
+ls -la /home/ubuntu/src/dreambooth/examples/dreambooth/text-inversion-model
+echo "/home/ubuntu/node/scripts/"
+ls -la /home/ubuntu/node/scripts/
