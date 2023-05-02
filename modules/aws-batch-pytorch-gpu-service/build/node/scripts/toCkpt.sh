00f1d3a19ebdc63592a05e94752c4ba886104302
schematical <mlea@schematical.com>
2023-05-02 12:46:31 -0500

    Finishing the rough model training data structure

--- modules/aws-batch-pytorch-gpu-service/build/node/scripts/toCkpt.sh
+++ modules/aws-batch-pytorch-gpu-service/build/node/scripts/toCkpt.sh
@@ -4,4 +4,4 @@ model_path=$1
 ckpt_name=$(basename $model_path)
 ckpt_path="${ckpt_name}.ckpt"
 
-python convertToCkpt.py --model_path=$model_path --checkpoint_path=$ckpt_path
+python3 convertToCkpt.py --model_path=$model_path --checkpoint_path=$ckpt_path
