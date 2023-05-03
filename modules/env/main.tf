9adea9aec15a1078a9cb454055ba480c97c0e548
schematical <mlea@schematical.com>
2023-05-03 17:13:31 -0500

    Finishing the rough model training data structure

--- modules/env/main.tf
+++ modules/env/main.tf
@@ -72,7 +72,7 @@ resource "aws_s3_bucket_cors_configuration" "dreambooth_storage_bucket" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["PUT", "POST"]
-    allowed_origins = ["https://s3-website-test.hashicorp.com"]
+    allowed_origins = ["*"] ## https://s3-website-test.hashicorp.com"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
