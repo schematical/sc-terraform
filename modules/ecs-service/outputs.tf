26fa1d4ac0e879a8efe2172d6fd221b45e4305c7
Your Name <you@example.com>
2023-09-28 14:13:23 -0500

    Migrating SOGOTP

--- modules/ecs-service/outputs.tf
+++ modules/ecs-service/outputs.tf
@@ -1,6 +1,9 @@
 output "ecs_task_execution_iam_role_arn" {
   value    = aws_iam_role.task_iam_role.arn
 }
+output "ecs_task_execution_iam_role_name" {
+  value    = aws_iam_role.task_iam_role.name
+}
 output "ecs_task_definition_arn" {
   value    = aws_ecs_task_definition.ecs_task_definition.arn
 }
