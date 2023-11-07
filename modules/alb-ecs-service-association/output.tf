c9bcc8ca3e802970b7eb8a308c5dc2b0ceed8ac7
Your Name <you@example.com>
2023-11-07 09:38:58 -0600

    Making ecs task definition command optional

--- modules/alb-ecs-service-association/output.tf
+++ modules/alb-ecs-service-association/output.tf
@@ -1,8 +1,8 @@
 output "aws_lb_target_group_arn" {
   value = aws_lb_target_group.alb_target_group.arn
 }
-output "aws_lb_target_group_id" {
-  value = aws_lb_target_group.alb_target_group.id
+output "aws_lb_target_group_arn_suffix" {
+  value = aws_lb_target_group.alb_target_group.arn_suffix
 }
 output "aws_lb_target_group_name" {
   value = aws_lb_target_group.alb_target_group.name
