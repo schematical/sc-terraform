3d981869c239d94c765a759b079516236daf63a9
Your Name <you@example.com>
2024-02-15 11:49:04 -0600

    Playing with the Diagrams

--- tf/projects/schematical_com/main.tf
+++ tf/projects/schematical_com/main.tf
@@ -197,6 +197,48 @@ resource "aws_dynamodb_table" "dynamodb_table_post" {
     Name        = "schematical-com"
   }
 }
+
+
+resource "aws_dynamodb_table" "dynamodb_table_user" {
+  name           = "SchematicalComUser"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "Username"
+
+  attribute {
+    name = "Username"
+    type = "S"
+  }
+
+  /*  attribute {
+      name = "Title"
+      type = "S"
+    }
+
+    attribute {
+      name = "Body"
+      type = "S"
+    }*/
+
+  /*
+    ttl {
+      attribute_name = "TimeToExist"
+      enabled        = false
+    }*/
+
+  /* global_secondary_index {
+     name               = "GameTitleIndex"
+     hash_key           = "GameTitle"
+     range_key          = "TopScore"
+     write_capacity     = 10
+     read_capacity      = 10
+     projection_type    = "INCLUDE"
+     non_key_attributes = ["UserId"]
+   }*/
+
+  tags = {
+    Name        = "schematical-com"
+  }
+}
 resource "aws_dynamodb_table" "dynamodb_table_diagram" {
   name           = "SchematicalComDiagram"
   billing_mode   = "PAY_PER_REQUEST"
@@ -349,6 +391,7 @@ module "dev_env_schematical_com" {
   secrets = var.env_info.dev.secrets
   dynamodb_table_arns = [
     aws_dynamodb_table.dynamodb_table_post.arn,
+    aws_dynamodb_table.dynamodb_table_user.arn,
     aws_dynamodb_table.dynamodb_table_diagram.arn,
     aws_dynamodb_table.dynamodb_table_diagram_object.arn,
     aws_dynamodb_table.dynamodb_table_map_flow.arn
@@ -375,7 +418,9 @@ module "prod_env_schematical_com" {
   secrets = var.env_info.prod.secrets
   dynamodb_table_arns = [
     aws_dynamodb_table.dynamodb_table_post.arn,
+    aws_dynamodb_table.dynamodb_table_user.arn,
     aws_dynamodb_table.dynamodb_table_diagram.arn,
-    aws_dynamodb_table.dynamodb_table_diagram_object.arn
+    aws_dynamodb_table.dynamodb_table_diagram_object.arn,
+    aws_dynamodb_table.dynamodb_table_map_flow.arn
   ]
 }
