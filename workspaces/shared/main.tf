6a3b8f372f28b515d6f377eee4e89d4197da4b95
Matt Lea <matt@schematical.com>
2025-08-17 16:22:19 -0500

    Playing with refactoring for real workspaces.

--- workspaces/shared/main.tf
+++ workspaces/shared/main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "schematical2-terraform-v1"
+    region = "us-east-1"
+    key    = "sc-workspaces-shared/terraform.tfstate"
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
